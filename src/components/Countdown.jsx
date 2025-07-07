5dbf9281b094fc98d3a868cf9e2bec375281c853
AdityaPatel2710 <ap8154036084@gmail.com>
2025-07-07 16:21:53 +0530

    Complted logic & Added basic styling

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,4 +1,19 @@
 import { useEffect, useRef, useState } from "react";
+import '../styles/Countdown.css';
+
+
+const getDisplayTime = (ms) => {
+  const secondsTotal = Math.floor(ms / 1000);
+
+  const days    = Math.floor(secondsTotal / (24 * 3600));
+  const hours   = Math.floor((secondsTotal % (24 * 3600)) / 3600);
+  const minutes = Math.floor((secondsTotal % 3600) / 60);
+  const seconds = secondsTotal % 60;
+
+  return { days, hours, minutes, seconds };
+};
+
+
 
 function Countdown() {
     const [target, setTarget] = useState(null);
@@ -34,6 +49,13 @@ function Countdown() {
         }, 1000);
     }
 
+    function handleStop() {
+        setDiff(0);
+        clearInterval(intervalRef.current);
+    }
+
+    const timeObj = getDisplayTime(diff);
+
     return (
         <div className="countdown-app-wrapper">
 
@@ -45,16 +67,15 @@ function Countdown() {
                     onChange = {(e) => setTarget(e.target.value)}
                 />
                 <button className="start-btn" onClick={handleStart} > Start </button>
+                {(diff != 0) && <button className="stop-btn" onClick={handleStop} > Stop </button>}
             </div>
-
-            {diff}
 
             <div className="timer-display-wrapper">
                 <ul>
-                    <li> <span className="days"></span> Days </li>
-                    <li> <span className="hours"></span> Hours </li>
-                    <li> <span className="minutes"></span> Minutes </li>
-                    <li> <span className="seconds"></span> Seconds </li>
+                    <li> <span className="days">{timeObj.days}</span> Days </li>
+                    <li> <span className="hours">{timeObj.hours}</span> Hours </li>
+                    <li> <span className="minutes">{timeObj.minutes}</span> Minutes </li>
+                    <li> <span className="seconds">{timeObj.seconds}</span> Seconds </li>
                 </ul>
             </div>
 
